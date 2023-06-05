f96764561f2b095bdebf42acfd067e8aa2c2a65d
JoseEPina <86281203+JoseEPina@users.noreply.github.com>
2023-06-05 10:41:50 +0200

    inititalized object arrays for the characters needed in the password criteria.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ interface and it is responsive, ensuring that it adapts to multiple screen sizes
 ## User Story
 
 ```
-- AS AN employee with access to senstive data
+- AS AN employee with access to sensitive data
 - I WANT to randomly generate a password that meets certain criteria
 - SO THAT I can create a strong password that provides greater security
 ```
--- script.js
+++ script.js
@@ -1,4 +1,131 @@
 // Assignment code here
+//* Constant needed here to ensure that these are NOT changed
+const criteriaNumber = 4;
+
+// Object array assignments needed here to store the each of
+// the password criteria needed for each selection.
+//! Variable needed here to create an Object Array assignment
+//! for the password Criteria
+// Criteria object:
+// name - Criteria Name
+// selected - was this criteria selected for password (yes/no)
+// totalChars - random # of total criteria characters TO BE used in password
+// assignedChars - current # of criteria characters used in password
+// setOfChars - defines set of the different characters to be used FOR THIS criteria
+
+var criteria = [
+    {
+        name: 'lowercase',
+        selected: false,
+        totalChars: 0,
+        assignedChars: 0,
+        setOfChars: [
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z',
+        ],
+    },
+    {
+        name: 'uppercase',
+        selected: false,
+        totalChars: 0,
+        assignedChars: 0,
+        setOfChars: [
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'H',
+            'I',
+            'J',
+            'K',
+            'L',
+            'M',
+            'N',
+            'O',
+            'P',
+            'Q',
+            'R',
+            'S',
+            'T',
+            'U',
+            'V',
+            'W',
+            'X',
+            'Y',
+            'Z',
+        ],
+    },
+    {
+        name: 'special-characters',
+        selected: false,
+        totalChars: 0,
+        assignedChars: 0,
+        setOfChars: [
+            '!',
+            '(',
+            ')',
+            '-',
+            '.',
+            '?',
+            '[',
+            ']',
+            '_',
+            '`',
+            '~',
+            ';',
+            ':',
+            '@',
+            '#',
+            '$',
+            '%',
+            '^',
+            '&',
+            '*',
+            '+',
+            '=',
+        ],
+    },{name: 'numeric',
+selected: false,
+totalChars: 0,
+assignedChars: 0, setofChars:[ '0','1','2','3','4', '5','6','7','8','9'],},
+];
+
+function generatePassword() {
+    var specialPrompt = confirm('Would you like to include special characters in your password?');
+    console.log('inside gereratePassword func');
+    if (specialPrompt === true) {
+        console.log(specialPrompt);
+    } else if (specialPrompt === false) {
+        console.log(specialPrompt);
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
