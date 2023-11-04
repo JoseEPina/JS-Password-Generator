f0c4a43b31a8555db3d3122d8493f8a9c3ed555b
JoseEPina <86281203+JoseEPina@users.noreply.github.com>
2023-11-05 00:36:53 +0100

    Intialized character types arrays.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Assignment code here
 //* Constant needed here to ensure that these are NOT changed
-const CRITERIA_NUMBER = 4;
 const PWD_MIN_LENGTH = 8;
 const PWD_MAX_LENGTH = 128;
+const NUMBER_OF_CRITERIA = 4;
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['!', '(', ')', '-', '.', '?', '[', ']', '_', '`', '~', ';', ':', '@', '#', '$', '%', '^', '&', '*', '+', '=',];
 
 // Object array assignments needed here to store the each of
 // the password criteria needed for each selection.
@@ -121,27 +126,34 @@ var criteria2 = [
     },
 ];
 //* Prompts user for pwd length and validates it within range
-function getLengthCriteria(pwdLength) {
+function getLengthCriteria() {
     while (pwdLength > PWD_MAX_LENGTH || pwdLength < PWD_MIN_LENGTH) {
         pwdLength = prompt('Please enter password length. Length should be between 8 to 128 characters.');
     }
     return pwdLength;
 }
 
-function getCharTypes(criteriaSelection) {
 
+function getCharTypes() {
+    for (var i = 0; i < criteriaSelection.length; i++) {
+        criteriaSelection[i] = confirm('Do you want to use ' + criteriaType[i] + ' to create a password?');
+    }
 }
 
+var criteriaSelection = [false, false, false, false]
+var criteriaType = ['uppercase letters', 'lowercase letters', 'special characters', 'numeric characters']
+var pwdLength = 0;
 // function needed to ask user for password criteria
 function askCriteria() {
     //TODO Ask for the length
-    var pwdLength = 0;
-    pwdLength = getLengthCriteria(pwdLength); // Gets and validates the length criteria
+    pwdLength = getLengthCriteria(); // Gets and validates the length criteria
     console.log('My Value:', pwdLength);
     //TODO Ask for the types of characters
-    var criteriaSelection = { uppercase: false, lowercase: false, numeric: false, specialChar: false }
+    n
     getCharTypes();
-    //TODO Validation of criteria selection
+    console.log(criteriaType);
+    console.log(criteriaSelection);
+
     //TODO Then generate the password
     //TODO Then Display the password
     // var specialPrompt = confirm('Would you like to include special characters in your password?');
@@ -156,7 +168,13 @@ function askCriteria() {
 
 
 function generatePassword() {
-    console.log('inside generatePassword func');
+    for (var i = 0; i < pwdLength; i++) {
+        // select criteria randomly 
+        // if (random criteriaSelection = true){ 
+        //
+
+    }
+}
 }
 
 
